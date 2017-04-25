b1a6f25f907a89ee9a13bc39fde70c425a604e7f
chmielny <michal.chmielo@gmail.com>
2017-04-25 21:41:18 +0200

    zprmonitor:
    -przejscie na boost:sloty/sygnaly
    -usuniecie timer interface

--- code/include/DaemonInterface.hpp
+++ code/include/DaemonInterface.hpp
@@ -1,16 +1,16 @@
 #ifndef DAEMONINTERFACE_HPP_
 #define DAEMONINTERFACE_HPP_
-#include <vector>
-#include "DaemonObserver.hpp"
+#include<boost/signal.hpp>
 
+typedef boost::signal<void (int)> Signal;
+typedef boost::function<void (int)> Slot;
 class DaemonInterface {
 private:
-//	std::vector<DaemonObserver> daemonObserverCollection_;
-    std::vector<DaemonObserver> daemonCollection_;
+	Signal signal_;
+	int value;
 public:
+	void connect();//Slot);
 	int getActValue();
-	int registerObserver();
-	int unregisterObserver();
 	int notifyObservers();
 };
 
--- code/include/Timer.hpp
+++ code/include/Timer.hpp
@@ -1,9 +1,13 @@
 #ifndef TIMER_HPP_
 #define TIMER_HPP_
+#include <boost>
 
-class Timer : public TimerInterface {
+typedef signal<void (int)> SignalTime;
+typedef boost::function<void (int)> SlotTime
 
+class Timer {
+public:
+	void notifyObservers();
+	void connect(SlotTime);
 };
-
-
 #endif //TIMER_HPP_
