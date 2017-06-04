1a712ce1b480d43113e1cb54c0b37e3b76d777f4
chmielny <michal.chmielo@gmail.com>
2017-06-04 20:38:54 +0200

    ramdaemon:
    -wersja linuch / winda

--- code/include/RamDaemon.hpp
+++ code/include/RamDaemon.hpp
@@ -1,9 +1,19 @@
 #ifndef RAMDAEMON_HPP_
 #define RAMDAEMON_HPP_
+#include "sys/types.h"
+#include "sys/sysinfo.h"
+#include "DaemonInterface.hpp"
 
 class RamDaemon : public DaemonInterface {
 public:
+	int getActValue();
 	void getDaemonInstance();
+
+#ifndef _WINDOWS
+private:
+    struct sysinfo memInfo_;
+#endif
+
 };
 
 
---
+++ code/src/RamDaemon.cpp
@@ -0,0 +1,34 @@
+#include "../include/RamDaemon.hpp"
+#include <iostream>
+
+#ifndef _WINDOWS
+int RamDaemon::getActValue() {
+    if(sysinfo(&memInfo_) == 0) {
+        long long totalPhysMem = memInfo_.totalram * memInfo_.mem_unit;
+        long long physMemUsed = (memInfo_.totalram - memInfo_.freeram) * memInfo_.mem_unit;
+        return physMemUsed * 100 / totalPhysMem;
+    } else {
+        std::cerr << "Get meminfo failed" << std::endl;
+        return -1;
+    } 
+}
+#endif
+
+#ifndef _WINDOWS
+#include "windows.h"
+int RamDaemon::getActValue() {
+    MEMORYSTATUSEX memInfo;
+    memInfo.dwLength = sizeof(MEMORYSTATUSEX);
+    if (memInfo.dwLength != 0) {
+        GlobalMemoryStatusEx(&memInfo);
+        DWORDLONG totalVirtualMem = memInfo.ullTotalPageFile;
+        DWORDLONG totalPhysMem = memInfo.ullTotalPhys;
+        DWORDLONG physMemUsed = memInfo.ullTotalPhys - memInfo.ullAvailPhys;
+        return physMemUsed * 100 / totalPhysMem;
+    }
+    else {
+        std::cerr << "memInfo failed" << std::endl;
+        return -1
+    }
+}
+#endif
