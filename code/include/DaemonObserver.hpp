61295fdb9031865e4d1323cfbca7a9dfd58d5125
chmielny <michal.chmielo@gmail.com>
2017-06-10 20:53:15 +0200

    zprmonitor
    doxygen

--- code/include/DaemonObserver.hpp
+++ code/include/DaemonObserver.hpp
@@ -1,3 +1,14 @@
+/**
+ * @file
+ *
+ * \class DaemonObserver
+ * Klasa DaemonObserver jest klasa bazowa dla wszystkich obserwatorow (overrunn, underrun, inrange, averageoverrun, averageunderrun).
+ * Metoda operator() kazdej z klas dziedziczacych wywoluje metode update(). W metodzie update sprawdzany jest warunek obserwatora
+ * np przekroczenie parametru i ew. wywolanie funkcji spod wskaznika userFunctionPtr_ - czyli wskaznika zarejestrowanego z zewnatrz.
+ * Operator == jest konieczny dla metody disconnect() z boost signals2. Cala klasa jest traktowana jako funktor podlaczany do daemona
+ * np badajacego CPU. Jezeli chcemy odlaczyc obserwatora, a do daemona jest podlaczonych kilku obserwatorow to musimy wiedziec jak porownac
+ * usuwany slot ze slotami listy (czyli obiektami DaemonObserver). Po to jest wlasnie operator ==.
+ */
 #ifndef DAEMONOBSERVER_HPP_
 #define DAEMONOBSERVER_HPP_
 #include <functional>
