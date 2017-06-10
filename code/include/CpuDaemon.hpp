62847a6266994090f688fcfe421e9f1123633bfc
chmielny <michal.chmielo@gmail.com>
2017-06-10 21:11:32 +0200

    zprmonit
    doxy

--- code/include/CpuDaemon.hpp
+++ code/include/CpuDaemon.hpp
@@ -1,3 +1,11 @@
+/**
+ * @file
+ *
+ * \class CpuDaemon
+ * Klasa ktorej obiekty - daemony sprawdzaja przez metode doMeasure() zuzycie procesora w %.
+ * Zawiera kod dla Windows / Linux. Metoda doMeasure() wysyla takze sygnal do zarejestrowanych slotow - obserwatorow.
+ */
+
 #ifndef CPUDAEMON_HPP_
 #define CPUDAEMON_HPP_
 #include "DaemonInterface.hpp"
